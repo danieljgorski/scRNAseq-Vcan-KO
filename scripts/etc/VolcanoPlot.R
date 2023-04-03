47bee13145d01f728365b61f8333feb00919a32f
danieljgorski <danieljgorski@gmail.com>
2023-04-03 15:45:22 +0200

    lower top n

--- scripts/etc/VolcanoPlot.R
+++ scripts/etc/VolcanoPlot.R
@@ -1,6 +1,6 @@
 # This produces a Volcano plot with statistically significant p < 0.01 genes
-# colored red (up-regulated) and blue (down-regulated). The top 20 most
-# statistically significant and top 20 most highly regulated genes
+# colored red (up-regulated) and blue (down-regulated). The top 5 most
+# statistically significant and top 5 most highly regulated genes
 # with an absolute average log2  fold change above 0.25  are labeled with
 # their gene symbol.
 
@@ -20,20 +20,20 @@ VolcanoPlot <- function(df, identity, title) {
   deg_up <- deg[deg$regulation == "Up", ]
   deg_up_sig <- deg_up[deg_up$p_val_adj < 0.01, ]
   deg_up_sig_fc <- deg_up_sig[deg_up_sig$avg_log2FC > 0.25, ]
-  deg_up_sig_fc_top20sig <- deg_up_sig_fc %>% slice_min(p_val_adj, n = 20)
-  deg_up_sig_fc_top20fc <- deg_up_sig_fc %>% slice_max(avg_log2FC, n = 20)
-  deg_up_tops <- rbind(deg_up_sig_fc_top20sig, deg_up_sig_fc_top20fc)
+  deg_up_sig_fc_topsig <- deg_up_sig_fc %>% slice_min(p_val_adj, n = 6)
+  deg_up_sig_fc_topfc <- deg_up_sig_fc %>% slice_max(avg_log2FC, n = 6)
+  deg_up_tops <- rbind(deg_up_sig_fc_topsig, deg_up_sig_fc_topfc)
+  deg_up_tops <- distinct(deg_up_tops)
   deg_up_tops <- distinct(deg_up_tops)
 
   # filter significantly downregulated genes
   deg_down <- deg[deg$regulation == "Down", ]
   deg_down_sig <- deg_down[deg_down$p_val_adj < 0.01, ]
   deg_down_sig_fc <- deg_down_sig[deg_down_sig$avg_log2FC < -0.25, ]
-  deg_down_sig_fc_top20sig <- deg_down_sig_fc %>% slice_min(p_val_adj, n = 20)
-  deg_down_sig_fc_top20fc <- deg_down_sig_fc %>% slice_min(avg_log2FC, n = 20)
-  deg_down_tops <- rbind(deg_down_sig_fc_top20sig, deg_down_sig_fc_top20fc)
+  deg_down_sig_fc_topsig <- deg_down_sig_fc %>% slice_min(p_val_adj, n = 6)
+  deg_down_sig_fc_topfc <- deg_down_sig_fc %>% slice_min(avg_log2FC, n = 6)
+  deg_down_tops <- rbind(deg_down_sig_fc_topsig, deg_down_sig_fc_topfc)
   deg_down_tops <- distinct(deg_down_tops)
-
 
   # volcano plot
   vp <- ggplot(
@@ -48,43 +48,38 @@ VolcanoPlot <- function(df, identity, title) {
       data = deg_up_sig,
       colour = "#eb2a0e"
     ) +
-    geom_point(
-      data = deg_down_sig,
-      colour = "#2664ad"
-    ) +
-    geom_text_repel(
+    geom_label_repel(
       data = deg_up_tops,
       aes(label = gene),
       colour = "#eb2a0e",
+      fill = "white",
       fontface = "italic",
-      size = 3,
-      force = 2,
-      nudge_x = max(abs(deg$avg_log2FC)),
-      nudge_y = median(abs(deg$neglog10p)),
-      direction = "both",
+      size = 6,
+      force = 6,
+      #nudge_x = ((max(abs(deg$avg_log2FC)))/4),
+      #nudge_y = 58,
       segment.size = 0.1,
       segment.alpha = 0.3,
-      segment.curvature = 0,
-      max.overlaps = Inf
+      max.overlaps = Inf) +
+   geom_point(
+      data = deg_down_sig,
+      colour = "#2664ad"
     ) +
-    geom_text_repel(
+    geom_label_repel(
       data = deg_down_tops,
       aes(label = gene),
       colour = "#2664ad",
+      fill = "white",
       fontface = "italic",
-      size = 3,
+      size = 6,
       force = 2,
-      nudge_x = -(max(abs(deg$avg_log2FC))),
-      nudge_y = median(abs(deg$neglog10p)),
-      direction = "both",
+      #nudge_x = (-(max(abs(deg$avg_log2FC)))/3),
+      #nudge_y = 80,
       segment.size = 0.1,
       segment.alpha = 0.3,
-      segment.curvature = 0,
-      max.overlaps = Inf
-    ) +
+      max.overlaps = Inf) +
     xlab(expression("avg log"[2] * "(FC)")) +
-    # ylab("-log10 (p-adj)") +
-    ylab(expression("-log"[10] * "(p-adj)")) +
+    ylab(expression("-log"[10] * "(P"[adj] * ")")) +
     xlim(
       -max(abs(deg$avg_log2FC)),
       max(abs(deg$avg_log2FC))
@@ -95,19 +90,12 @@ VolcanoPlot <- function(df, identity, title) {
       color = "grey",
       size = 0.1
     ) +
-    # geom_vline(xintercept = -0.25,
-    #            linetype="dashed",
-    #            color = "grey",
-    #            size = 0.1) +
-    # geom_vline(xintercept = 0.25,
-    #            linetype="dashed",
-    #            color = "grey",
-    #            size = 0.1) +
     theme_bw() +
     theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
-      axis.title = element_text(size = 14)
+      axis.title = element_text(size = 18),
+      plot.title = element_text(size = 18)
     ) +
     ggtitle(title)
   print(vc)
