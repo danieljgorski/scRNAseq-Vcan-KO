27c02401e0d8bb66c230d4ecd056bfd400280850
danieljgorski <danieljgorski@gmail.com>
2023-04-03 15:44:45 +0200

    Hmmr-OE to Vcan-KO customization

--- scripts/16-misc-figures.R
+++ scripts/16-misc-figures.R
@@ -1,18 +1,28 @@
 # Miscellaneous figures for the manuscript
 
 # Load libraries
-library(Seurat) #>=4.0.1
-library(scater)
+library(Seurat)
 library(stringr)
 library(readr)
 library(ggplot2)
 library(ggpubr)
 library(rstatix)
 library(ggrepel)
-library(viridis)
 source("scripts/etc/colors.R")
 source("scripts/etc/geno_colors.R")
 source("scripts/etc/dimplotlevels.R")
+
+# Set up output dirs
+output_dirs <- c("results",
+                 "results/misc-figures")
+for (i in output_dirs) {
+  if (!dir.exists(i)) {
+    dir.create(i)
+    print(paste0("made ", i, " directory"))
+  } else {
+    print(paste0(i, " directory already exists."))
+  }
+}
 
 # Load object, slim down and save
 load("results/objects/obj_annotated.Rdata")
@@ -21,75 +31,60 @@ save(obj, file = "results/objects/obj_annotated_diet.Rdata")
 
 # Metadata to df for ggplot2 QC plots
 df <- obj@meta.data
-df$timepoint <- factor(df$timepoint, levels = c("day_3", "day_7"),
-                       labels = c("3 d", "7 d"))
-df$sample <- factor(df$sample, levels = c("S47_B5_WT",
-                                          "S47_B6_WT",
-                                          "S48_R5_WT",
-                                          "S47_B1_OE",
-                                          "S47_B3_OE",
-                                          "S48_R1_OE",
-                                          "S48_R3_OE",
-                                          "S18_G2_WT",
-                                          "S18_G3_WT",
-                                          "S18_G8_WT",
-                                          "S18_G1_OE",
-                                          "S18_G6_OE",
-                                          "S18_G7_OE",
-                                          "S18_R1_OE"))
+df$genotype <- factor(df$genotype, levels = c("Vcan-WT", "Vcan-KO"),
+                       labels = c("WT", "KO"))
+df$sample <- factor(df$sample, levels = c("Blau5Control",
+                                          "Rot2Control",
+                                          "Blau4KO",
+                                          "Rot4KO",
+                                          "Rot5KO"))
 # nFeature Plot
-p <- ggplot(data = df, aes(x=sample, y=nFeature_RNA, fill = timepoint)) +
+p <- ggplot(data = df, aes(x=sample, y=nFeature_RNA, fill = genotype)) +
   geom_violin() +
   labs(x="Sample", y="nFeature") +
-  scale_fill_manual(values = c("#F8BC24", "#F58800")) +
+  scale_fill_manual(values = c("#999999", "#55A0FB")) +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.text = element_text(colour = "black", size = 10),
-        axis.title = element_text(size = 14),
+        axis.text = element_text(colour = "black", size = 20),
+        axis.title = element_text(size = 24),
         legend.title = element_blank(),
-        legend.text = element_text(size = 10))
+        legend.text = element_text(size = 20))
 p
 pdf(file = "results/misc-figures/nFeature.pdf",
-    height = 3.5,
-    width = 7,
     useDingbats = F)
 print(p)
 dev.off()
 
 # nCount Plot
-p <- ggplot(data = df, aes(x=sample, y=nCount_RNA, fill = timepoint)) +
+p <- ggplot(data = df, aes(x=sample, y=nCount_RNA, fill = genotype)) +
   geom_violin() +
   labs(x="Sample", y="nCount") +
-  scale_fill_manual(values = c("#F8BC24", "#F58800")) +
+  scale_fill_manual(values = c("#999999", "#55A0FB")) +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.text = element_text(colour = "black", size = 10),
-        axis.title = element_text(size = 14),
+        axis.text = element_text(colour = "black", size = 20),
+        axis.title = element_text(size = 24),
         legend.title = element_blank(),
-        legend.text = element_text(size = 10))
+        legend.text = element_text(size = 20))
 p
 pdf(file = "results/misc-figures/nCount.pdf",
-    height = 3.5,
-    width = 7,
     useDingbats = F)
 print(p)
 dev.off()
 
 # percent.mt Plot
-p <- ggplot(data = df, aes(x=sample, y=percent.mt, fill = timepoint)) +
+p <- ggplot(data = df, aes(x=sample, y=percent.mt, fill = genotype)) +
   geom_violin() +
   labs(x="Sample", y="% mitochondrial reads") +
-  scale_fill_manual(values = c("#F8BC24", "#F58800")) +
+  scale_fill_manual(values = c("#999999", "#55A0FB")) +
   theme_classic() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        axis.text = element_text(colour = "black", size = 10),
-        axis.title = element_text(size = 14),
+        axis.text = element_text(colour = "black", size = 20),
+        axis.title = element_text(size = 24),
         legend.title = element_blank(),
-        legend.text = element_text(size = 10))
+        legend.text = element_text(size = 20))
 p
 pdf(file = "results/misc-figures/percent_mt.pdf",
-    height = 3.5,
-    width = 7,
     useDingbats = F)
 print(p)
 dev.off()
@@ -105,13 +100,13 @@ p <- DimPlot(obj,
   xlab("UMAP-1") +
   ylab("UMAP-2") +
   labs(color = "Identity") +
-  theme(legend.text = element_text(size = 20),
-        legend.title = element_text(size = 24),
+  theme(legend.text = element_text(size = 14),
+        legend.title = element_text(size = 16),
         legend.justification = "top",
         legend.key.size = unit(3, "point"),
         axis.text.x = element_blank(),
         axis.text.y = element_blank(),
-        axis.title = element_text(size = 32),
+        axis.title = element_text(size = 18),
         plot.title = element_blank()) +
   guides(color = guide_legend(override.aes = list(size = 4.25),
                               nrow = 37))
@@ -121,7 +116,7 @@ q <- LabelClusters(plot = p,
                    force = 0.25,
                    box = T,
                    fill = alpha("white", 0.45),
-                   size = 6,
+                   size = 5,
                    label.r = unit(0.25, "lines"),
                    label.size = NA)
 pdf(file = "results/misc-figures/DimPlot_basic_annotation.pdf",
@@ -130,159 +125,35 @@ pdf(file = "results/misc-figures/DimPlot_basic_annotation.pdf",
 print(q)
 dev.off()
 
-
-# DimPlot with correct sizing, split by time point
-obj@meta.data$timepoint <- factor(obj@meta.data$timepoint,
-                                  levels = c("day_3", "day_7"),
-                                  labels = c("3 d", "7 d"))
-p <- DimPlot(obj,
-             reduction = "umap",
-             pt.size = .3,
-             raster = F,
-             label = T,
-             label.size = 5,
-             cols = colors,
-             group.by = "basic_annotation",
-             split.by = "timepoint") +
-  xlab("UMAP-1") +
-  ylab("UMAP-2") +
-  labs(color = "Identity") +
-  theme(legend.text = element_text(size = 18),
-        legend.title = element_text(size = 24),
-        legend.justification = "top",
-        legend.key.size = unit(3, "point"),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.title = element_text(size = 32),
-        plot.title = element_blank(),
-        strip.text.x = element_text(size = 22)) +
-  guides(color = guide_legend(override.aes = list(size = 3.25),
-                              nrow = 37))
-p
-pdf(file = "results/misc-figures/DimPlot_basic_annotation_timepoint_split.pdf",
-    useDingbats = F,
-    width = 16)
-print(p)
-dev.off()
-
-# DimPlot with correct sizing, split by genotype
-obj@meta.data$genotype <- factor(obj@meta.data$genotype,
-                                  levels = c("WT", "OE"),
-                                  labels = c("WT", "Hmmr-OE"))
-p <- DimPlot(obj,
-             reduction = "umap",
-             pt.size = .3,
-             raster = F,
-             label = T,
-             label.size = 5,
-             cols = colors,
-             group.by = "basic_annotation",
-             split.by = "genotype") +
-  xlab("UMAP-1") +
-  ylab("UMAP-2") +
-  labs(color = "Identity") +
-  theme(legend.text = element_text(size = 18),
-        legend.title = element_text(size = 24),
-        legend.justification = "top",
-        legend.key.size = unit(3, "point"),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.title = element_text(size = 32),
-        plot.title = element_blank(),
-        strip.text.x = element_text(size = 22)) +
-  guides(color = guide_legend(override.aes = list(size = 3.25),
-                              nrow = 37))
-p
-pdf(file = "results/misc-figures/DimPlot_basic_annotation_genotype_split.pdf",
-    useDingbats = F,
-    width = 16)
-print(p)
-dev.off()
-
-
-# OE Hmmr feature plot
-f <- FeaturePlot(subset(obj, genotype == "OE"),
-            features = "Hmmr",
-            reduction = "umap",
-            pt.size = .3,
-            raster = F) & scale_color_viridis_c(limits = c(0, 5)) 
-f <- f + labs(x = "UMAP-1", y = "UMAP-2", title = expression(paste(italic("Hmmr"), "-OE")), col = "Hmmr") +
-  theme(legend.text = element_text(size = 14),
-        legend.title = element_text(size = 20, face = "italic"),
-        legend.key.size = unit(.75, "cm"),
-        legend.position = "right",
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.title = element_text(size = 32),
-        plot.title = element_text(size = 50, hjust = 0))
-f
-pdf(file = "results/misc-figures/featureplot_hmmr-oe.pdf",
-    useDingbats = F,
-    width = 8)
-print(f)
-dev.off()
-
-# WT Hmmr feature plot
-f <- FeaturePlot(subset(obj, genotype == "WT"),
-                 features = "Hmmr",
-                 reduction = "umap",
-                 pt.size = .3,
-                 raster = F) & scale_color_viridis_c(limits = c(0, 5)) 
-f <- f + labs(x = "UMAP-1", y = "UMAP-2", title = expression(paste("WT")), col = "Hmmr") +
-  theme(legend.text = element_text(size = 14),
-        legend.title = element_text(size = 20, face = "italic"),
-        legend.key.size = unit(.75, "cm"),
-        legend.position = "right",
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.title = element_text(size = 32),
-        plot.title = element_text(size = 50, hjust = 0))
-f
-pdf(file = "results/misc-figures/featureplot_hmmr-wt.pdf",
-    useDingbats = F,
-    width = 8)
-print(f)
-dev.off()
-
-# Hmmr split vln plot
-v <- VlnPlot(obj, features = "Hmmr",
-        split.by = "genotype",
-        cols = c("#999999", "#008000"),
-        pt.size = 0,
-        raster = F,
-        split.plot = T) +
-  theme(plot.title = element_text(size = 50, face = "italic", hjust = 0),
-        legend.text = element_text(size = 24),
-        axis.title = element_text(size = 32),
-        axis.text = element_text(size = 24))
-pdf(file = "results/misc-figures/vlnplot_hmmr-split.pdf",
-    useDingbats = F,
-    width = 16)
-print(v)
-dev.off()
-
-# Feature plots of canonical markers, sce style
-sce <- as.SingleCellExperiment(obj, assay = "RNA")
-markers <- read.csv(file = "data/canonical_markers.csv")
-markers <- markers$All
-for (i in markers) {
-  p <- plotReducedDim(sce,
-                      dimred = "UMAP",
-                      colour_by = i,
-                      by_exprs_values = "logcounts") +
-    scale_fill_viridis_b() +
+# Feature plots of canonical markers
+gois <- c("Col1a1", "Wt1", "Dmkn", "Tcf21", "Pdgfra", "Gsn",
+          "Vcan", "Postn", "Cthrc1", "Mki67")
+for (i in gois) {
+  p <- FeaturePlot(obj, features = i, cols = c("lightgrey", "#fe3108")) +
     labs(x = "UMAP-1", y = "UMAP-2", title = i) +
     theme(axis.text = element_blank(),
           legend.key.size = unit(.75, "cm"),
           legend.position = c(0.01,0.95),
           legend.direction = "horizontal",
           legend.title = element_blank(),
-          plot.title = element_text(size = 50, face = "italic"),
+          plot.title = element_text(size = 40, face = "italic", hjust = 0),
           legend.spacing.y = unit(0.15, "cm"),
           legend.text = element_text(size = 14),
-          axis.title = element_text(size = 32))
+          axis.title = element_text(size = 24))
   pdf(file = paste0("results/misc-figures/featureplot_", i, ".pdf"),
       useDingbats = F)
   print(p)
   dev.off()
 }
+
+# VlnPlot of Vcan expression
+v <- VlnPlot(obj, features = "Vcan", cols = colors, pt.size = 0) +
+  labs(y = expression(paste(italic("Vcan"), " expression level"))) +
+  theme(legend.position = "none",
+        axis.title.x = element_blank(),
+        plot.title = element_blank())
+pdf(file = "results/misc-figures/VlnPlot_Vcan.pdf",
+    useDingbats = F,
+    height = 3.5)
+print(v)
+dev.off()
